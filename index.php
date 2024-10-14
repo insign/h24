b19f911459bba79673e99ee4bdb698c697460bbd
Hélio <insign@gmail.com>
2024-10-13 22:12:12 -0300

    Versão 2024.2.14

--- index.php
+++ index.php
@@ -23,6 +23,27 @@ if (is_home() && !is_paged()) {
   else:
     echo '<p>' . __('Nenhum post encontrado na categoria "articles".', 'h24') . '</p>';
   endif;
+
+  // Argumentos para a consulta
+  $args = [
+    'category_name' => 'ideas',
+    'posts_per_page' => 3,
+  ];
+  $blog_query = new WP_Query($args);
+
+  if ($blog_query->have_posts()): ?>
+    <h1><?php _e('Invenções Recentes', 'h24'); ?></h1>
+    <?php
+    while ($blog_query->have_posts()):
+      $blog_query->the_post(); ?>
+      <div class="post">
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      </div>
+    <?php endwhile;
+    wp_reset_postdata();
+  else:
+    echo '<p>' . __('Nenhum post encontrado na categoria "ideas".', 'h24') . '</p>';
+  endif;
 } else {
   // Outros tipos de páginas (ex: arquivo, busca)
   if (have_posts()):
