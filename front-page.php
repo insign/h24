c5940ad42b60987b7408d7a12cbf9adf28e4d5b6
Hélio <insign@gmail.com>
2024-10-15 13:52:48 -0300

    Versão 2024.2.33

--- front-page.php
+++ front-page.php
@@ -11,11 +11,8 @@
   if ($blog_query->have_posts()): ?>
     <h1><?php _e('Artigos Recentes', 'h24'); ?></h1>
     <?php
-    while ($blog_query->have_posts()):
-      $blog_query->the_post(); ?>
-      <div class="content">
-        <h2><span class="date"><?php echo get_the_date(); ?> - </span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      </div>
+    while ($blog_query->have_posts()): $blog_query->the_post(); ?>
+        <h2 class='entry-title'><span class="date"><?php echo get_the_date(); ?> - </span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php endwhile;
     wp_reset_postdata();
   else:
@@ -32,11 +29,8 @@
   if ($blog_query->have_posts()): ?>
     <h1><?php _e('Invenções Recentes', 'h24'); ?></h1>
     <?php
-    while ($blog_query->have_posts()):
-      $blog_query->the_post(); ?>
-      <div class="content">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      </div>
+    while ($blog_query->have_posts()): $blog_query->the_post(); ?>
+        <h2 class='entry-title'><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php endwhile; wp_reset_postdata();
   else:
     echo '<p>' . __('Nenhum ideia encontrada, algo está errado.', 'h24') . '</p>';
