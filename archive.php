ead5b21d60426f927d7768235ba4f8eb7ab0b695
Hélio <insign@gmail.com>
2024-10-15 13:08:28 -0300

    Versão 2024.2.22

--- archive.php
+++ archive.php
@@ -1,7 +1,6 @@
-<?php get_header(); ?>
+<?php get_header() ?>
 <?php if (have_posts()) : ?>
-  <h1>
-    <?php
+  <h1><?php
     if (is_category()) :
       single_cat_title();
     elseif (is_tag()) :
@@ -13,29 +12,28 @@
     elseif (is_day()) :
       printf(__('Dia %s', 'h24'), get_the_date());
     elseif (is_month()) :
-      printf(__('Mês: %s', 'h24'), get_the_date('F Y'));
+      printf(__('M s: %s', 'h24'), get_the_date('F Y'));
     elseif (is_year()) :
       printf(__('Ano: %s', 'h24'), get_the_date('Y'));
     else :
       _e('Arquivos', 'h24');
     endif;
-    ?>
-  </h1>
+    ?></h1>
 
   <?php while (have_posts()) : the_post() ?>
     <?php if (is_page()) : ?>
-      <h2 class="entry-title"><?php the_title(); ?></h2>
+      <h2 class="entry-title"><?= get_the_title() ?></h2>
     <?php else : ?>
-      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> - <span class='date'><?php the_date(); ?></span></h2>
-    <?php endif; ?>
-  <?php endwhile; ?>
+      <h2 class="entry-title"><a href="<?= get_the_permalink() ?>"><?= get_the_title() ?></a> - <span class='date'><?= get_the_date() ?></span></h2>
+    <?php endif ?>
+  <?php endwhile ?>
 
   <?php if ($GLOBALS['wp_query']->max_num_pages > 1) : ?>
-    <div class="pagination"><?php the_posts_pagination(); ?></div>
-  <?php endif; ?>
+    <div class="pagination"><?= get_the_posts_pagination() ?></div>
+  <?php endif ?>
 
 <?php else : ?>
-  <p><?php _e('Nada encontrado.', 'h24'); ?></p>
-<?php endif; ?>
+  <p><?= __('Nada encontrado.', 'h24') ?></p>
+<?php endif ?>
 
-<?php get_footer(); ?>
+<?php get_footer() ?>
