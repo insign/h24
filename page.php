71b0f4c4d1fecf03d0e88b89f92cf15704b1b222
Hélio <insign@gmail.com>
2024-10-15 13:39:31 -0300

    Versão 2024.2.30

--- page.php
+++ page.php
@@ -1,3 +1,8 @@
 <?php get_header() ?>
-  <h1 class='entry-title'><?= get_the_title() ?></h1>
+<h1 class='entry-title'><?= get_the_title() ?></h1>
+
+<h2 class='date'><?= get_the_date() ?></h2>
+<div class="entry-content">
+  <?= get_the_content() ?>
+</div>
 <?php get_footer() ?>
