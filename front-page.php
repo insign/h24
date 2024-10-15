cd1c0507d407c1606d3e9e8a9d2112b21295ce93
Hélio <insign@gmail.com>
2024-10-15 13:22:09 -0300

    Versão 2024.2.23

---
+++ front-page.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+<?php
+  // Argumentos para a consulta
+  $args = [
+    'category_name' => 'articles',
+    'posts_per_page' => 3,
+  ];
+  $blog_query = new WP_Query($args);
+
+  if ($blog_query->have_posts()): ?>
+    <h1><?php _e('Artigos Recentes', 'h24'); ?></h1>
+    <?php
+    while ($blog_query->have_posts()):
+      $blog_query->the_post(); ?>
+      <div class="post">
+        <h2><span><?php echo get_the_date(); ?> - </span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      </div>
+    <?php endwhile;
+    wp_reset_postdata();
+  else:
+    echo '<p>' . __('Nenhum artigo encontrado, algo está errado.', 'h24') . '</p>';
+  endif;
+
+  // Argumentos para a consulta
+  $args = [
+    'category_name' => 'ideas',
+    'posts_per_page' => 3,
+  ];
+  $blog_query = new WP_Query($args);
+
+  if ($blog_query->have_posts()): ?>
+    <h1><?php _e('Invenções Recentes', 'h24'); ?></h1>
+    <?php
+    while ($blog_query->have_posts()):
+      $blog_query->the_post(); ?>
+      <div class="post">
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      </div>
+    <?php endwhile;
+    wp_reset_postdata();
+  else:
+    echo '<p>' . __('Nenhum ideia encontrada, algo está errado.', 'h24') . '</p>';
+  endif;
+?>
+
+<?php get_footer(); ?>
--- index.php
+++ index.php
@@ -1,70 +1,29 @@
-<?php get_header(); ?>
+<?php get_header() ?>
 
 <?php
-// Verificar se está na página inicial
-if (is_home() && !is_paged()) {
-  // Argumentos para a consulta
-  $args = [
-    'category_name' => 'articles',
-    'posts_per_page' => 3,
-  ];
-  $blog_query = new WP_Query($args);
-
-  if ($blog_query->have_posts()): ?>
-    <h1><?php _e('Artigos Recentes', 'h24'); ?></h1>
-    <?php
-    while ($blog_query->have_posts()):
-      $blog_query->the_post(); ?>
-      <div class="post">
-        <h2><span><?php echo get_the_date(); ?> - </span><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      </div>
-    <?php endwhile;
-    wp_reset_postdata();
-  else:
-    echo '<p>' . __('Nenhum post encontrado na categoria "articles".', 'h24') . '</p>';
-  endif;
-
-  // Argumentos para a consulta
-  $args = [
-    'category_name' => 'ideas',
-    'posts_per_page' => 3,
-  ];
-  $blog_query = new WP_Query($args);
-
-  if ($blog_query->have_posts()): ?>
-    <h1><?php _e('Invenções Recentes', 'h24'); ?></h1>
-    <?php
-    while ($blog_query->have_posts()):
-      $blog_query->the_post(); ?>
-      <div class="post">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      </div>
-    <?php endwhile;
-    wp_reset_postdata();
-  else:
-    echo '<p>' . __('Nenhum post encontrado na categoria "ideas".', 'h24') . '</p>';
-  endif;
-} else {
   // Outros tipos de páginas (ex: arquivo, busca)
   if (have_posts()):
     while (have_posts()):
-      the_post(); ?>
+      the_post() ?>
       <div class="post">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> - <span class="date"><?php echo get_the_date(); ?></span></h2>
+        <?php if (is_page()) : ?>
+          <h2 class="entry-title"><?= get_the_title() ?></h2>
+        <?php else : ?>
+          <h2 class="entry-title"><a href="<?= get_the_permalink() ?>"><?= get_the_title() ?></a> - <span class='date'><?= get_the_date() ?></span></h2>
+        <?php endif ?>
         <div class="entry-content">
-          <?php the_content(); ?>
+          <?= get_the_content() ?>
         </div>
       </div>
     <?php endwhile; ?>
     <?php if ( $GLOBALS['wp_query']->max_num_pages > 1 ) : ?>
       <div class="pagination">
-        <?php the_posts_pagination(); ?>
+        <?= get_the_posts_pagination() ?>
       </div>
     <?php endif; ?>
   <?php else:
     echo '<p>' . __('Nenhum post encontrado.', 'h24') . '</p>';
   endif;
-}
 ?>
 
-<?php get_footer(); ?>
+<?php get_footer() ?>
