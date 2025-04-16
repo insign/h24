5f9c57aca906ee4b484b471b9dd07a2523692dfa
helio <open@helio.me>
2025-04-15 21:44:42 -0300

    feat: Adiciona atributo async="async" ao script de comentários Giscus

--- single.php
+++ single.php
@@ -11,7 +11,7 @@
   </article>
 
 <comments>
-   <script src='//giscus.app/client.js' data-repo='insign/helio.me-comments' data-repo-id='R_kgDOOaRcaQ' data-category='Announcements' data-category-id='DIC_kwDOOaRcac4CpIkq' data-mapping='pathname' data-strict='0' data-reactions-enabled='1' data-emit-metadata='1' data-input-position='top' data-theme='preferred_color_scheme' data-lang='pt' data-loading='lazy' crossorigin='anonymous' async></script>
+   <script src='//giscus.app/client.js' data-repo='insign/helio.me-comments' data-repo-id='R_kgDOOaRcaQ' data-category='Announcements' data-category-id='DIC_kwDOOaRcac4CpIkq' data-mapping='pathname' data-strict='0' data-reactions-enabled='1' data-emit-metadata='1' data-input-position='top' data-theme='preferred_color_scheme' data-lang='pt' data-loading='lazy' crossorigin='anonymous' async="async"></script>
 </comments>
 
 <?php get_footer(); ?>
